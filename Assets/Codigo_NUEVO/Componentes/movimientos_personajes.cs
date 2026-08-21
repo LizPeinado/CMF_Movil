9717d34a31d1ac790469ceba718eceda79db56d4
unknown <hoiimendless@gmail.com>
2026-08-20 21:09:06 -0600

    Mover ataques a controlador general

--- Assets/Codigo_NUEVO/Componentes/movimientos_personajes.cs
+++ Assets/Codigo_NUEVO/Componentes/movimientos_personajes.cs
@@ -17,16 +17,23 @@ public class movimientos_personajes : MonoBehaviour
     public bool esta_saltando = false;
 
     //Recibir HITBOXES dependiendo personaje
-    [Header("Hitboxes")]
+    /*[Header("Hitboxes")]
     public GameObject hitbox_delantera_completa;
     public GameObject hitbox_trasera_completa;
 
     public GameObject hitbox_delantera_agachado;
     public GameObject hitbox_trasera_agachado;
 
-    public GameObject hitbox_cabeza;
+    public GameObject hitbox_cabeza;*/
 
-   
+    private bool siguiente_golpe_izquierdo = true;
+
+    public GolpesManos puño_izquierdo;
+    public GolpesManos puño_derecho;
+
+    public GolpesManos pie_izquierdo;
+    public GolpesManos pie_derecho;
+
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
@@ -104,7 +111,37 @@ public class movimientos_personajes : MonoBehaviour
         // !! CAMBIAR HITBOXES
     }
 
+    public void hacer_golpe_debil(Animator animator)
+    {
+        if(siguiente_golpe_izquierdo)
+        {
+            animator.SetTrigger("Golpe2");
+        }
+        else
+        {
+            animator.SetTrigger("Golpe3");
+        }
 
+        siguiente_golpe_izquierdo = !siguiente_golpe_izquierdo;
+        StartCoroutine(activar_ataque(puño_izquierdo,puño_derecho,10,0.2f));
+    }
+
+    public void hacer_golpe_fuerte(Animator animator){
+        animator.SetTrigger("GolpeFuerte");
+        StartCoroutine(activar_ataque(puño_izquierdo,puño_derecho,30,1f));
+    }
+
+    public void hacer_patada_debil(Animator animator){
+        animator.SetTrigger("PatadaDebil");
+        
+        StartCoroutine(activar_ataque(pie_izquierdo,pie_derecho,20,1f));
+    }
+
+    public void hacer_patada_fuerte(Animator animator)
+    {
+        animator.SetTrigger("PatadaFuerte");
+        StartCoroutine(activar_ataque(pie_izquierdo,pie_derecho,60,0.35f));
+    }
 
     public void cambiarEstado(EstadosMovimiento estado_nuevo){
         estado_actual = estado_nuevo;
@@ -112,6 +149,19 @@ public class movimientos_personajes : MonoBehaviour
         if(hay_gente_escuchando_el_estado != null){
             hay_gente_escuchando_el_estado.Invoke(estado_nuevo);
         }
+    }
+    IEnumerator activar_ataque(GolpesManos golpe1,GolpesManos golpe2,int daño,float duracion)
+    {
+        golpe1.daño = daño;
+        golpe2.daño = daño;
+
+        golpe1.ataque_activo = true;
+        golpe2.ataque_activo = true;
+
+        yield return new WaitForSeconds(duracion);
+
+        golpe1.ataque_activo = false;
+        golpe2.ataque_activo = false;
     }
 
 }
--- Assets/Codigo_NUEVO/Controles/controlador_jugador.cs
+++ Assets/Codigo_NUEVO/Controles/controlador_jugador.cs
@@ -1,17 +1,35 @@
 using System;
+using System.Collections;
+using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
 public class ControladorJugador : movimientos_personajes{
     private Rigidbody rigid__body;
     private PlayerInput entradas_jugador;
+    
+    //INPUT ACTIONS - MOVIMIENTOS
     private InputAction movimientos;
     private InputAction saltando;
     private InputAction agachando;
 
+    //INPUT ACTIONS - ATAQUES
+    private InputAction golpe_debil;
+    private InputAction golpe_fuerte;
+    private InputAction patada_debil;
+    private InputAction patada_fuerte;
+
+    //Manos y Pies Para ataques (limpiar al rato)
+    
+
     //RECIBIR INFORMACION DE PERSONAJE
     public float velocidad_movimiento = 5.0f;
+
+    private Animator animator_jugador;
     //cuando se tenga, minimo empezar con Jose
+
+    //INFORMACION DE ENEMIGO
+    private List<GameObject> a_quien_madreamos;
 
 
 
@@ -20,6 +38,7 @@ public class ControladorJugador : movimientos_personajes{
         //Agarrando componentes pa que jale
         entradas_jugador = GetComponent<PlayerInput>();
         rigid__body = GetComponent<Rigidbody>();
+        animator_jugador = GetComponent<Animator>();
 
         //Declarando movimientos principales
         movimientos = entradas_jugador.actions.FindAction("movimiento");
@@ -29,6 +48,39 @@ public class ControladorJugador : movimientos_personajes{
         agachando = entradas_jugador.actions.FindAction("Agacharse");
         agachando.performed += agachar;
         agachando.canceled += no_se_agache;
+
+        //Declarar ataques
+        golpe_debil = entradas_jugador.actions.FindAction("GolpeDebil");
+        golpe_fuerte = entradas_jugador.actions.FindAction("GolpeFuerte");
+
+        patada_debil = entradas_jugador.actions.FindAction("PatadaDebil");
+        patada_fuerte = entradas_jugador.actions.FindAction("PatadaFuerte");
+
+        golpe_debil.performed += dale_golpe_debil;
+        golpe_fuerte.performed += dale_golpe_fuerte;
+
+        patada_debil.performed += dale_patada_debil;
+        patada_fuerte.performed += dale_patada_fuerte;
+
+        //Buscar a quien pegar
+    }
+
+    void dale_golpe_debil(InputAction.CallbackContext _){
+        hacer_golpe_debil(animator_jugador);
+        
+    }
+
+    void dale_golpe_fuerte(InputAction.CallbackContext _){
+        hacer_golpe_fuerte(animator_jugador);
+        
+    }
+
+    void dale_patada_debil(InputAction.CallbackContext _){
+        hacer_patada_debil(animator_jugador);   
+    }
+
+    void dale_patada_fuerte(InputAction.CallbackContext _){
+        hacer_patada_fuerte(animator_jugador);
     }
 
     void saltar(InputAction.CallbackContext _){
@@ -84,5 +136,13 @@ public class ControladorJugador : movimientos_personajes{
         if(direccion.magnitude > 0.1f){
             avanzar_adelante(direccion, velocidad_movimiento);
         }
+
+        
     }
+
+    //Para los Ataques
+
+
+
+
 }
